164b2ea06b382bea04db5eff0e8d1552f8f0f9eb
Ivan Krylov <ordaswingson@gmail.com>
2019-08-12 17:21:36 +0300

    -первый коммит
    Signed-off-by: Ivan Krylov <ordaswingson@gmail.com>

--- Simpl creator/Form1.Designer.cs
+++ Simpl creator/Form1.Designer.cs
@@ -28,12 +28,145 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.создатьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.новоеРешениеSimplToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.загрузитьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.сохранитьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.выходToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.сборкаToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.собратьРешениеToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.сохранитьToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem1,
+            this.сборкаToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(915, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.создатьToolStripMenuItem,
+            this.загрузитьToolStripMenuItem,
+            this.сохранитьToolStripMenuItem1,
+            this.сохранитьToolStripMenuItem,
+            this.выходToolStripMenuItem});
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(48, 20);
+            this.toolStripMenuItem1.Text = "Фаил";
+            this.toolStripMenuItem1.Click += new System.EventHandler(this.toolStripMenuItem1_Click);
+            // 
+            // создатьToolStripMenuItem
+            // 
+            this.создатьToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.новоеРешениеSimplToolStripMenuItem});
+            this.создатьToolStripMenuItem.Name = "создатьToolStripMenuItem";
+            this.создатьToolStripMenuItem.Size = new System.Drawing.Size(153, 22);
+            this.создатьToolStripMenuItem.Text = "Создать";
+            // 
+            // новоеРешениеSimplToolStripMenuItem
+            // 
+            this.новоеРешениеSimplToolStripMenuItem.Name = "новоеРешениеSimplToolStripMenuItem";
+            this.новоеРешениеSimplToolStripMenuItem.Size = new System.Drawing.Size(195, 22);
+            this.новоеРешениеSimplToolStripMenuItem.Text = "Новое решение Simpl";
+            this.новоеРешениеSimplToolStripMenuItem.Click += new System.EventHandler(this.новоеРешениеSimplToolStripMenuItem_Click);
+            // 
+            // загрузитьToolStripMenuItem
+            // 
+            this.загрузитьToolStripMenuItem.Name = "загрузитьToolStripMenuItem";
+            this.загрузитьToolStripMenuItem.Size = new System.Drawing.Size(153, 22);
+            this.загрузитьToolStripMenuItem.Text = "Открыть";
+            this.загрузитьToolStripMenuItem.Click += new System.EventHandler(this.загрузитьToolStripMenuItem_Click);
+            // 
+            // сохранитьToolStripMenuItem
+            // 
+            this.сохранитьToolStripMenuItem.Name = "сохранитьToolStripMenuItem";
+            this.сохранитьToolStripMenuItem.Size = new System.Drawing.Size(153, 22);
+            this.сохранитьToolStripMenuItem.Text = "Сохранить как";
+            this.сохранитьToolStripMenuItem.Click += new System.EventHandler(this.сохранитьToolStripMenuItem_Click);
+            // 
+            // выходToolStripMenuItem
+            // 
+            this.выходToolStripMenuItem.Name = "выходToolStripMenuItem";
+            this.выходToolStripMenuItem.Size = new System.Drawing.Size(153, 22);
+            this.выходToolStripMenuItem.Text = "Выход";
+            // 
+            // сборкаToolStripMenuItem
+            // 
+            this.сборкаToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.собратьРешениеToolStripMenuItem});
+            this.сборкаToolStripMenuItem.Name = "сборкаToolStripMenuItem";
+            this.сборкаToolStripMenuItem.Size = new System.Drawing.Size(60, 20);
+            this.сборкаToolStripMenuItem.Text = "Сборка";
+            // 
+            // собратьРешениеToolStripMenuItem
+            // 
+            this.собратьРешениеToolStripMenuItem.Name = "собратьРешениеToolStripMenuItem";
+            this.собратьРешениеToolStripMenuItem.Size = new System.Drawing.Size(173, 22);
+            this.собратьРешениеToolStripMenuItem.Text = "Собрать решение";
+            this.собратьРешениеToolStripMenuItem.Click += new System.EventHandler(this.собратьРешениеToolStripMenuItem_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.richTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.richTextBox1.Location = new System.Drawing.Point(12, 27);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(891, 341);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            this.richTextBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.richTextBox1_KeyDown);
+            // 
+            // сохранитьToolStripMenuItem1
+            // 
+            this.сохранитьToolStripMenuItem1.Name = "сохранитьToolStripMenuItem1";
+            this.сохранитьToolStripMenuItem1.Size = new System.Drawing.Size(153, 22);
+            this.сохранитьToolStripMenuItem1.Text = "Сохранить";
+            this.сохранитьToolStripMenuItem1.Click += new System.EventHandler(this.сохранитьToolStripMenuItem1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(915, 380);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem сохранитьToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem загрузитьToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem создатьToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem выходToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem сборкаToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem собратьРешениеToolStripMenuItem;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.ToolStripMenuItem новоеРешениеSimplToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem сохранитьToolStripMenuItem1;
     }
 }
 
--- Simpl creator/Form1.cs
+++ Simpl creator/Form1.cs
@@ -3,8 +3,10 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -12,9 +14,130 @@ namespace Simpl_creator
 {
     public partial class Form1 : Form
     {
+        string name;
+        string path;
         public Form1()
         {
             InitializeComponent();
+            name = "NewCommand";
+            this.Text = name;
+        }
+
+        private void toolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void собратьРешениеToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if(name != "")
+            {
+                System.Diagnostics.Process.Start("SML Studio.exe", name);
+            }
+            else
+            {
+                MessageBox.Show("фаил не сохранен");
+            }
+
+            
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void richTextBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Space || e.KeyCode == Keys.Enter)
+            {
+                desighn();
+            }
+        }
+
+        private void загрузитьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog OPF = new OpenFileDialog();
+            OPF.DefaultExt = "sc";
+            OPF.Filter = "Simpl(*.sc)|*.sc";
+            if (OPF.ShowDialog() == DialogResult.OK)
+            {
+                name = OPF.FileName;
+                using (StreamReader streamReader = new StreamReader(OPF.FileName))
+                {
+                    richTextBox1.Clear();
+                    while (!streamReader.EndOfStream)
+                    {
+                        richTextBox1.Text += streamReader.ReadLine() + Environment.NewLine;
+                    }
+                }
+                desighn();
+                this.Text = name;
+                path = OPF.FileName;
+            }
+        }
+
+        private void сохранитьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog SPF = new SaveFileDialog();
+            SPF.DefaultExt = "sc";
+            SPF.Filter = "Simpl(*.sc)|*.sc";
+            if (SPF.ShowDialog() == DialogResult.OK)
+            {
+                this.Text = name = SPF.FileName;
+                path = SPF.FileName;
+                using (StreamWriter writer = new StreamWriter(SPF.FileName))
+                {
+                    writer.Write(richTextBox1.Text);
+                }
+
+            }
+                       
+
+        }
+
+        private void новоеРешениеSimplToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Clear();
+            this.Text = "NewCommand";
+        }
+
+        private void desighn()
+        {
+            var currentSelStart = richTextBox1.SelectionStart; //получаем начальную позицию
+            var currentSelLength = richTextBox1.SelectionLength; //получаем конечную позицию
+
+            richTextBox1.SelectAll(); //выделяем весь текст
+            richTextBox1.SelectionColor = SystemColors.WindowText;
+
+            var matches = Regex.Matches(richTextBox1.Text, @"let|goto|flag|print|input|if|rem|end|var|text");
+            foreach (var match in matches.Cast<Match>())
+            {
+                if (match.ToString() == "rem")
+                {
+                    int lineIndex = richTextBox1.GetLineFromCharIndex(match.Index);
+                    richTextBox1.Select(match.Index, richTextBox1.Lines[lineIndex].Length);
+                    richTextBox1.SelectionColor = Color.Green;
+                }
+                else
+                {
+                    richTextBox1.Select(match.Index, match.Length);
+                    richTextBox1.SelectionColor = Color.Blue;
+                }
+
+            }
+
+            richTextBox1.Select(currentSelStart, currentSelLength);
+            richTextBox1.SelectionColor = SystemColors.WindowText;
+        }
+
+        private void сохранитьToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+
+            using (StreamWriter writer = new StreamWriter(path))
+            {
+                writer.Write(richTextBox1.Text);
+            }
         }
     }
 }
